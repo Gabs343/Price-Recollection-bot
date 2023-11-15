cfd903699699c19c86ea3bfa83375be900ddc911
Gabs343 <gabriellopezp343@gmail.com>
2023-11-15 19:57:18 -0300

    calling methods from the bna object

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+
+from settings import *
+from processes.bna import BnaProcess
+        
+class Main:
+    __settings_services: list[SettingService] = []
+    __bot_name = "BOT_1"
+    __status = "READY"
+    
+    def __init__(self) -> None:
+        for setting in (SettingSap, SettingBNA, SettingBCRA):
+            self.__settings_services.append(setting())
+    
+    @property   
+    def settings_services(self) -> list[SettingService]:
+        return self.__settings_services
+    
+    @property   
+    def bot_name(self) -> str:
+        return self.__bot_name
+    
+    @property   
+    def status(self) -> str:
+        return self.__status
+        
+    def start(self) -> None:
+        
+        bna = BnaProcess()
+        bna.open()
+        print(bna.get_foreign_bills_for(self.get_setting(SettingBNA).settings["Foreign_Bills"]))
+        print(bna.get_foreign_exchanges_for(self.get_setting(SettingBNA).settings["Foreign_Exchange"]))
+        bna.close()
+
+            
+    def get_setting(self, setting_type: SettingService) -> SettingService:
+        #gives StopIteration Exception
+        return next(setting for setting in self.settings_services if isinstance(setting, setting_type))
+    
+
+if __name__ == "__main__":
+    st = time.time()
+    main = Main()
+    main.start()
+    et = time.time()
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
+    
