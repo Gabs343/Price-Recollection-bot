d88c8f0adff72bc2a434831b6798b72e96d36c20
Gabs343 <gabriellopezp343@gmail.com>
2023-11-20 19:20:00 -0300

    refactor

--- processes/bna.py
+++ processes/bna.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
-from settings import SettingBNA
-
 class BnaProcess:
     def __init__(self) -> None:
         self.url = "https://www.bna.com.ar/Personas"
